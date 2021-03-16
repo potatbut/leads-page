6ef062a970d63628b376bfc0aa14eaa92f4d10b0
vera <darthyoon@gmail.com>
2021-03-16 18:38:54 +0300

    Slick slider fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as $ from 'jquery'
 window.$ = window.jQuery = $
 import 'slick-carousel'
 import './styles/styles.scss'
-
+import 'slick-carousel/slick/slick.min';
 
 $('.single-item').slick({
   fade: true,
